508553e0bd74d0cf388a2d26c5d1a2a56d62e0a6
Patrick Bethmann <patrickbethmann@outlook.de>
2021-11-10 20:43:07 +0100

    minor improvements

--- src/API.js
+++ src/API.js
@@ -1,9 +1,12 @@
+import Weather from './Weather';
+
 async function getWeather(location, units) {
   let response = await fetch(
     `http://api.openweathermap.org/data/2.5/weather?q=${location}&units=${units}&appid=afe27c4e375966c49eaea24984167756`
   );
-  let weather = await response.json();
-  console.log(weather);
+  let weatherData = await response.json();
+
+  let weatherObject = new Weather(weatherData);
 }
 
 export { getWeather };
--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 import './style.css';
 import * as API from './API';
 import * as Display from './Display';
+
+API.getWeather('brunswick, DE', 'metric');

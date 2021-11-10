03432c0522e190939763b5c82db06549d5be69b0
Patrick Bethmann <patrickbethmann@outlook.de>
2021-11-10 20:11:49 +0100

    Added getWeather() Function

--- src/API.js
+++ src/API.js
@@ -1,3 +1,9 @@
 async function getWeather(location, units) {
-  let response = fetch('api.openweathermap.org/data/2.5/weather?q={city name}&appid=afe27c4e375966c49eaea24984167756');
+  let response = await fetch(
+    `http://api.openweathermap.org/data/2.5/weather?q=${location}&units=${units}&appid=afe27c4e375966c49eaea24984167756`
+  );
+  let weather = await response.json();
+  console.log(weather);
 }
+
+export { getWeather };

633d69c1e1a794735b855f981ca05d46030495da
Patrick Bethmann <patrickbethmann@outlook.de>
2021-11-21 20:08:35 +0100

    changed http to https in the api requests

--- src/API.js
+++ src/API.js
@@ -4,7 +4,7 @@ async function getWeather(location, units = 'metric') {
   console.log(location);
   console.log(units);
   let response = await fetch(
-    `http://api.openweathermap.org/data/2.5/weather?q=${location}&units=${units}&appid=afe27c4e375966c49eaea24984167756`
+    `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=${units}&appid=afe27c4e375966c49eaea24984167756`
   );
 
   let weatherData = await response.json();
